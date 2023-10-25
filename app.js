c43130f67bda6cc95a79b88c181b00365c70945b
Susan Williams <williams.susanj@gmail.com>
2023-10-25 14:50:34 +0100

    update app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import morgan from "morgan";
 
 import { frontendRoute } from "./routes/frontendRoute.js";
 import { backendRoute } from "./routes/backendRoute.js";
+import { funstuffRoute } from "./routes/funstuffRoute.js";
+import { uxdesignRoute } from "./routes/uxdesignRoute.js";
 export const app = express();
 
 app.use(morgan("dev"));
@@ -13,19 +15,5 @@ app.use(cors());
 
 app.use("/frontend", frontendRoute);
 app.use("/backend", backendRoute);
-app.use("funstuff", funstuffRoute)
-
-// app.get("/", (req, res) => {
-//   console.log("Hello")
-//   res.json({ status: "success", data: "This route works!" });
-// });
-
-// app.get("/frontend", getFrontendList);
-// app.post("/frontend", postFrontend)
-// app.delete("/frontend/:id", deleteFrontend);
-
-// const PORT = 7000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server listening on port ${PORT}`);
-// });
+app.use("/funstuff", funstuffRoute);
+app.use("/uxdesign", uxdesignRoute);
