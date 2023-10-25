3078365511cf3292415f54d55a77856a1c8799b6
MartiZu <martina.zurli@gmail.com>
2023-10-25 11:39:32 +0100

    working thunderclient

--- controllers/frontendController.js
+++ controllers/frontendController.js
@@ -3,7 +3,7 @@ import { getFrontend } from "../models/frontendModel.js";
 //HTTP handler
 export async function getFrontendList(req, res) {
   const frontendData = await getFrontend();
-  // res.status(200).json({ status: "success", data: frontendData });
+  res.status(200).send({ status: "success", data: frontendData });
   console.log(frontendData);
 }
 
