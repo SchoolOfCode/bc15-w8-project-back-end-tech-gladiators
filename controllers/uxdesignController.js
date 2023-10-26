412fc931a2cedbb9472f5c9ff9c5b8dfd1c80c74
MartiZu <martina.zurli@gmail.com>
2023-10-26 10:57:56 +0100

    handle errors users

--- controllers/uxdesignController.js
+++ controllers/uxdesignController.js
@@ -10,13 +10,12 @@ export async function getUxdesignList(req, res) {
     const UxdesignData = await getUxdesign();
     res.status(200).send({ status: "success", data: UxdesignData });
     console.log(UxdesignData);
-  } catch {
-    res
-      .status(500)
-      .send({
-        status: "error",
-        message: "An error occurred while fetching UX design data.",
-      });
+  } catch (error) {
+    // Handle the error, for example, by sending an error response.
+    res.status(500).send({
+      status: "error",
+      message: "An error occurred while fetching UX design data.",
+    });
     console.error(error);
   }
 }
@@ -30,29 +29,39 @@ export async function postUxdesign(req, res) {
     const newUxdesignData = await createUxdesign(newData);
     //return status
     res.status(201).json({ status: "success", data: newUxdesignData });
-  } catch {
-    res
-      .status(500)
-      .send({
-        status: "error",
-        message: "An error occurred while creating UX design data.",
-      });
+  } catch (error) {
+    // Handle the error, for example, by sending an error response.
+    res.status(500).send({
+      status: "error",
+      message: "An error occurred while creating UX design data.",
+    });
     console.error(error);
   }
 }
 
 //Endpoint to delete item by id
 export async function deleteUxdesign(req, res) {
-  //store in a variable your req.params.id
-  const id = req.params.id;
-  //store in a variable the await promise
-  const deleteItem = await deleteUxdesignById(id);
-  //404 error if item not found
-  if (!deleteItem) {
-    return res
-      .status(404)
-      .json({ status: "fail", data: "Data item not found by Id." });
+  try {
+    //store in a variable your req.params.id
+    const id = req.params.id;
+    //store in a variable the await promise
+    const deleteItem = await deleteUxdesignById(id);
+    //404 error if item not found
+    if (!deleteItem) {
+      return res
+        .status(404)
+        .json({ status: "fail", data: "Data item not found by Id." });
+    }
+    //return success status
+    res.status(200).json({ status: "success", data: deleteItem });
+  } catch (error) {
+    // Handle the error, for example, by sending an error response.
+    console.error(error);
+    res
+      .status(500)
+      .json({
+        status: "error",
+        message: "An error occurred while deleting the UX design data.",
+      });
   }
-  //return success status
-  res.status(200).json({ status: "success", data: deleteItem });
 }
