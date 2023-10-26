d4a6efce657dbf39b90f617228fa0633b0a93823
Olivia Johnson <olivia.1996@outlook.com>
2023-10-26 15:48:17 +0100

    hashing passwords

--- models/usersModel.js
+++ models/usersModel.js
@@ -1,16 +1,25 @@
 // Import the 'pool' object so our helper functions can interact with the PostgreSQL database
 import { pool } from "../db/index.js";
+import bcrypt, { hash } from "bcrypt";
+
+const saltRounds = 10;
 
 //get user by id
 export async function getUserByNameAndPassword(name, password) {
   // Query the database and return the user with a matching name and password
   //initiate variable to store our SQL query string
-  const queryText = "SELECT * FROM users WHERE name = $1 AND password= $2";
+  console.log(name, password)
+  const queryText = "SELECT * FROM users WHERE name = $1";
   //await the pool query to send the query to the database
-  const result = await pool.query(queryText, [name, password]);
+  const result = await pool.query(queryText, [name]);
   //return the user in the row with the specific details
   //otherwise our return will be NULL
-  return result.rows[0] || null;
+  let passwordCheck = result.rows[0].password
+  const isMatch = await bcrypt.compare(password, passwordCheck)
+  if(isMatch){
+    return result.rows[0]
+  } else {
+  return null;}
 }
 
 export async function createUser(user) {
@@ -18,11 +27,14 @@ export async function createUser(user) {
   const queryText =
     "INSERT INTO users (name, email, username, password) VALUES ($1, $2, $3, $4) RETURNING *";
   //define elements of the request and placeholder values
+  //hash password before storing
+  const hash = await bcrypt.hash(user.password, 10)
+
   const result = await pool.query(queryText, [
     user.name,
     user.email,
     user.username,
-    user.password,
+    hash,
   ]);
   //return result
   return result.rows[0] || null;
