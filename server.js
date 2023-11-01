6307525ca6dacb475151686c379b7848f706b0f6
Olivia Johnson <olivia.1996@outlook.com>
2023-11-01 14:20:34 +0000

    updated port with a default

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 import { app } from "./app.js";
 import 'dotenv/config'
 
-const PORT = process.env.PORT;
-console.log(process.env.PORT)
+const PORT = process.env.PORT ?? 6666;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
